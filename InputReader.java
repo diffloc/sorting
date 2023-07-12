2652a551b860c6118b47b984596668fdb5016db4
diffloc <6632742+diffloc@users.noreply.github.com>
2023-07-11 21:19:55 -0500

    Update InputReader.java

--- InputReader.java
+++ InputReader.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 public class InputReader {
 
     public static DataParts<String> readInput() {
-        Scanner scanner = new Scanner(System.in);
         DataParts<String> inputParts = new DataParts<>(new ArrayList<>());
-        while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            if (input.isEmpty()) {
-                break;
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                if (input.isEmpty()) {
+                    break;
+                }
+                inputParts.getParts().add(input);
             }
-            inputParts.getParts().add(input);
         }
-
         return inputParts;
     }
 }
